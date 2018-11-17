8f897aeea5190341cb46adeb9ad1569437ba70a1
bryannguyeen <bryan.nguyen@ucalgary.ca>
2018-11-17 11:33:30 -0700

    Added error checking for signup page
    Checks if username field is not empty
    Checks if passwords match
    Checks if password is at least 8 characters long

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const dbPromise = sqlite.open('./db.sqlite', { cached: true, Promise }).then(db 
 
 app.get('/', async (req, res, next) => {
     res.render('pages/index');
+
     /*
     try {
         const db = await dbPromise;
@@ -44,7 +45,17 @@ app.get("/login", async (req, res) => {
 })
 
 app.get("/signup", async (req, res) => {
-    res.render('pages/signuppage');
+    var errorMessage = "";
+    if (String(req.query.invalid) == "nomatch") {
+        errorMessage = "Passwords do not match";
+    }
+    if (req.query.invalid == "emptyusername") {
+        errorMessage = "Username can't be empty";
+    }
+    if (req.query.invalid == "tooshort") {
+        errorMessage = "Password must be at least 8 characters";
+    }
+    res.render('pages/signuppage', {message: errorMessage});
 })
 
 app.post("/accountcreation", (req, res) => {
@@ -58,12 +69,40 @@ app.post("/accountcreation", (req, res) => {
     console.log("Username: " + username);
     console.log("Password: " + password);
     console.log("Confirm Password: " + confirmPassword);
-    if (password == confirmPassword) {
-        console.log("Paswords match!")
+
+    // username is empty
+    if (String(username).length < 1) {
+        console.log("Username is empty!");
+        res.redirect('/signup?invalid=emptyusername');
+        return;
+    } else {
+        console.log("Username is not empty!")
+    }
+
+    // username contains an invalid character
+    // CODE HERE
+
+    // passwords don't match
+    if (password != confirmPassword) {
+        console.log("Passwords don't match!");
+        res.redirect('/signup?invalid=nomatch');
+        return;
     }
     else {
-        console.log("Passwords don't match!")
+        console.log("Paswords match!");
     }
+
+    // password is too short
+    if (String(password).length < 8) {
+        console.log("Password is too short!");
+        res.redirect('/signup?invalid=tooshort');
+        return;
+    } else {
+        console.log("Password is a good length!")
+    }
+
+    // if they pass all conditions, log the user in
+    res.redirect('/');
     res.end();
 })
 
