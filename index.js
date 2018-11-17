07d7a1202b1f19c75433da6d1fa787ec1006245f
bryannguyeen <bryan.nguyen@ucalgary.ca>
2018-11-17 00:58:44 -0700

    Added loginpage and signuppage

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const sqlite = require('sqlite');
 const config = require('./config');
 const ejs = require('ejs');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 if (!config.session.secret) {
     throw new Error('You must fill in the session secret in the config')
 }
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 app.use(session(Object.assign({
@@ -35,6 +38,34 @@ app.get('/', async (req, res, next) => {
         next(err);
     } */
 });
+
+app.get("/login", async (req, res) => {
+    res.render('pages/loginpage');
+})
+
+app.get("/signup", async (req, res) => {
+    res.render('pages/signuppage');
+})
+
+app.post("/accountcreation", (req, res) => {
+    console.log("trying to create user...");
+
+    // This is the information retrieved from the signup page
+    const username = req.body.create_username;
+    const password = req.body.create_password;
+    const confirmPassword = req.body.confirm_password;
+
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+    console.log("Confirm Password: " + confirmPassword);
+    if (password == confirmPassword) {
+        console.log("Paswords match!")
+    }
+    else {
+        console.log("Passwords don't match!")
+    }
+    res.end();
+})
 
 app.listen(port);
 
