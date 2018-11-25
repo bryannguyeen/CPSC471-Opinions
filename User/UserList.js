6e1681a60d28cddd6c977c95efa76ee86dc221a1
Spencer92 <smhmaslen@gmail.com>
2018-11-24 20:57:15 -0700

    events.js:167 throw er; //Unhandled \'error\' event... but we don't have an events.js, that\'s a problem

--- User/UserList.js
+++ User/UserList.js
@@ -11,27 +11,36 @@ class UserList
   {
     var user = User.setName(Username);
     user = User.setPassword(Password);
-    var createTable = 'CREATE TABLE IF NOT EXISTS LOGS (id user_table, username data_type PRIMARY KEY varchar(255), password varchar(255))';
+
+    let user = [user.getname(), user.getPassword()];
+
+    db.run('CREATE TABLE IF NOT EXISTS \'user\' (username varchar(20) NOT NULL, password varchar(20) NOT NULL, PRIMARY KEY (\'username\') )')
+
+
+//    var createTable = 'CREATE TABLE IF NOT EXISTS LOGS (id user_table, username data_type PRIMARY KEY varchar(255), password varchar(255))';
 //    var userName = 'username data_type PRIMARY KEY';
 //    var userPassword = 'password data_type NOT NULL';
 //    var createTable = createTable.concat('(', userName, ', ', userPassword, ', ', 'table_constraint) [WITHOUT ROWID]');
-    var insertInto = 'INSERT INTO user_table';
+/*    var insertInto = 'INSERT INTO user_table';
     var insertUserMeta = insertInto.concat(' ( ', 'username, ', 'password ) ', )
     var insertVal = 'VALUES';
-    var insertUser = insertVal.concat(' ( ', user.getname(), ', ', user.getPassword(), ' );' );
+    var insertUser = insertVal.concat(' ( ', user.getname(), ', ', user.getPassword(), ' );' );*/
+
 
 /*    var userCheck = 'IF NOT EXISTS (SELECT * FROM user.objects WHERE user = USER_TABLE(N\'user.usertables\') AND type in (N\'U\'))';
     var userCreate = 'CREATE TABLE user.usertables';
     var INSERT_INTO = 'INSERT_INTO'
     var USER_TABLE = 'USER_TABLE';*/
     var userInsert = INSERT_INTO.concat
-    db.transaction(function (tx)
+
+
+/*    db.transaction(function (tx)
                   {
                     tx.excuteSql(createTable);
-                    tx.excuteSql(insertUserMeta);
-                    tx.excuteSql(insertUser);
+//                    tx.excuteSql(insertUserMeta);
+//                    tx.excuteSql(insertUser);
                   }
-                );
+                );*/
 
 
   }
